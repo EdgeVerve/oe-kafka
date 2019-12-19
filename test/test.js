b32733cd674975dc68994823fef44b31b9704031
Ajith <ajith_v@edgeverve.com>
2019-12-19 15:47:28 +0530

    Increased test timeout

--- test/test.js
+++ test/test.js
@@ -89,7 +89,7 @@ producer.on('ready', function (err) {
 
 
 describe(chalk.blue('Kafka Mixin Test'), function (done) {
-  this.timeout(10000);
+  this.timeout(20000);
   before('waiting for boot scripts to complete', function (done) {
 
     app.on('test-start', function () {
